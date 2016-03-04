7343b1f314db0b80776c3091e9db26a1304124b0
Maximilien Bersoult <mbersoult@centreon.com>
2016-03-04 11:48:56 +0100

    Add license header

--- src/behat/CentreonContainer.php
+++ src/behat/CentreonContainer.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Copyright 2016 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  *  Run a Centreon container and manage it.
@@ -20,8 +35,9 @@ class CentreonContainer
         $ch = curl_init('http://localhost:' . $this->getPort());
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 500);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        while (!curl_exec($ch))
+        while (!curl_exec($ch)) {
             sleep(1);
+        }
         curl_close($ch);
     }
 
@@ -44,5 +60,3 @@ class CentreonContainer
         return shell_exec('docker port "' . $this->container_id . '" 80 | cut -d : -f 2 | tr -d "\n"');
     }
 }
-
-?>
