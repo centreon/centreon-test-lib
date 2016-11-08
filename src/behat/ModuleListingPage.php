ebff8891d949566bae7d59ceb25f97339c31f06b
Kevin Duret <kduret@centreon.com>
2016-11-08 15:43:59 +0100

    Add module listing page

---
+++ src/behat/ModuleListingPage.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright 2016 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Centreon\Test\Behat;
+
+class ModuleListingPage implements ListingPage
+{
+    private $context;
+
+    /**
+     *  Module list page.
+     *
+     *  @param $context  Centreon context class.
+     *  @param $visit    True to navigate to page.
+     */
+    public function __construct($context, $visit = TRUE)
+    {
+        // Visit page.
+        $this->context = $context;
+        if ($visit) {
+            $this->context->visit('main.php?p=50701');
+        }
+
+        // Check that page is valid for this class.
+        $mythis = $this;
+        $this->context->spin(function ($context) use ($mythis) {
+            return $mythis->isPageValid();
+        },
+        5,
+        'Current page does not match class ' . __CLASS__);
+    }
+
+    /**
+     *  Check that the current page is matching this class.
+     *
+     *  @return True if the current page matches this class.
+     */
+    public function isPageValid()
+    {
+        return $this->context->getSession()->getPage()->has('css', 'tr.ListHeader');
+    }
+
+    /**
+     *  Get the list of templates.
+     */
+    public function getEntries()
+    {
+        $entries = array();
+        $elements = $this->context->getSession()->getPage()->findAll('css', '.list_one,.list_two');
+        foreach ($elements as $element) {
+            $entry = array();
+            $entry['name'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(1) a')->getText();
+            $entry['realname'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(2) a')->getText();
+            $entry['description'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(3)')->getText();
+            $entry['version'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(4)')->getText();
+            $entry['author'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(5)')->getText();
+            $entry['expirationDate'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(6)')->getText();
+            $entry['installed'] = $this->context->assertFindIn($element, 'css', 'td:nth-child(7)')->getText();
+            $entry['actions'] = array(
+                'install' => false,
+                'upgrade' => false,
+                'remove' => false
+            );
+            $actionsComponent = $this->context->assertFindIn($element, 'css', 'td:nth-child(9)');
+            if ($actionsComponent->has('css', 'img[src$="generate_conf.png"]')) {
+                $entry['actions']['install'] = true;
+            }
+            if ($actionsComponent->has('css', 'img[src$="upgrade.png"]')) {
+                $entry['actions']['upgrade'] = true;
+            }
+            if ($actionsComponent->has('css', 'img[src$="delete.png"]')) {
+                $entry['actions']['remove'] = true;
+            }
+            $entries[$entry['name']] = $entry;
+        }
+        return $entries;
+    }
+
+}
