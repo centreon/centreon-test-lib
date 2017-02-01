14bce1daa798752308df4277a2b635bd57548359
Kevin Duret <kduret@centreon.com>
2017-02-01 15:45:54 +0100

    add openldap container

--- src/behat/Extensions/ContainerExtension.php
+++ src/behat/Extensions/ContainerExtension.php
@@ -53,6 +53,7 @@ class ContainerExtension implements ExtensionInterface
                 ->scalarNode('web')->defaultValue('mon-web-dev.yml')->end()
                 ->scalarNode('web_fresh')->defaultValue('mon-web-fresh-dev.yml')->end()
                 ->scalarNode('web_kb')->defaultValue('mon-web-kb-dev.yml')->end()
+                ->scalarNode('web_openldap')->defaultValue('mon-web-openldap-dev.yml')->end()
                 ->scalarNode('ppm_squid_simple')->defaultValue('mon-ppm-squid-simple-dev.yml')->end()
                 ->scalarNode('ppm_squid_basic_auth')->defaultValue('mon-ppm-squid-basic-auth-dev.yml')->end()
                 ->scalarNode('lm_squid_simple')->defaultValue('mon-lm-squid-simple-dev.yml')->end()
