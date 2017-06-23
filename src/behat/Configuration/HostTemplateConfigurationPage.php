51ce9a6d0910846d0d07aab921d569b128773bdb
mariegallardo <mgallardo@centreon.com>
2017-06-23 10:32:40 +0200

    Adding the status property to HostTemplateConfigurationPage

--- src/behat/Configuration/HostTemplateConfigurationPage.php
+++ src/behat/Configuration/HostTemplateConfigurationPage.php
@@ -141,7 +141,12 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
             'text',
             'input[name="host_acknowledgement_timeout"]',
             self::DATA_TAB
-        )
+        ),
+        //Extended tab
+        'status' => array(
+            'radio',
+            'input[name="host_activate[host_activate]"]',
+            self::EXTENDED_TAB
     );
 
     /**
