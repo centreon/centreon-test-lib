1a57e51c24e9ed5d922bbc1677f0034c91307072
Kev <duret.kevin@gmail.com>
2017-06-27 14:45:13 +0200

    Update LdapConfigurationListingPage.php

--- src/behat/Administration/LdapConfigurationListingPage.php
+++ src/behat/Administration/LdapConfigurationListingPage.php
@@ -8,18 +8,18 @@ class LdapConfigurationListingPage extends \Centreon\Test\Behat\ListingPage
     protected $validField = 'input[name="searchLdap"]';
     
     protected $properties = array(
-         'configuration_name' => array(
+        'configuration_name' => array(
             'text',
             'td:nth-child(2)'
-            ),
-         'id' => array(
+        ),
+        'id' => array(
             'custom'
-            ),
-         'status' => array(
+        ),
+        'status' => array(
             'text',
             'td:nth-child(4)'   
-            )
-        );
+        )
+    );
     
     protected $objectClass = '\Centreon\Test\Behat\Administration\LdapConfigurationPage';
     
