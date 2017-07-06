8daa1861df1e71368a132916e90bf1ca0de093b5
mariegallardo <gallardo.marie@hotmail.com>
2017-07-06 15:55:44 +0200

    advmultiselect and select handled (#27)
    * advmultiselect and select handled
    
    * Obsolete line deleted
    
    * File updated
    
    * File updated
    
    advmultiselect
    
    * File updated
    
    advmultiselect
    
    * File updated
    
    advmultiselect
    
    * improve advmultiselect getter
    
    * do not return empty advmultiselect
    
    * setProperties advmultiselect correctly handled

--- src/behat/Administration/ACLActionConfigurationPage.php
+++ src/behat/Administration/ACLActionConfigurationPage.php
@@ -32,7 +32,7 @@ class ACLActionConfigurationPage extends \Centreon\Test\Behat\ConfigurationPage
         ),
         'acl_groups' => array(
             'advmultiselect',
-            'select[name="acl_groups-f[]"]'
+            'acl_groups'
         ),
         'action_top_counter_overview' => array(
             'checkbox',
