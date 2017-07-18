c341054ecd60c56d86e2f8c4677eb60e512f3069
Matthieu Mandoula <casperton21@icloud.com>
2017-07-18 14:27:50 +0200

    virtualsMetrics (#36)
    * virtualsMetrics config pages created
    
    * properties css data updated
    
    * "function" property css data updated

---
+++ src/behat/Monitoring/MetricsConfigurationPage.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright 2016-2017 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Centreon\Test\Behat\Monitoring;
+
+class MetricsConfigurationPage extends \Centreon\Test\Behat\ConfigurationPage
+{
+    protected $validField = 'input[name="vmetric_name"]';
+
+    protected $properties = array(
+        'name' => array(
+            'input',
+            'input[name="vmetric_name"]'
+        ),
+        'linked-host_services' => array(
+            'input',
+            'input[name="cg_alias"]'
+        ),
+        'def_type' => array(
+            'select',
+            'select[name="def_type"]'
+        ),
+        'known_metrics' => array(
+            'select',
+            'select#sl_list_metrics'
+        ),
+        'function' => array(
+            'input',
+            'textarea[name="rpn_function"]'
+        ),
+        'hidden_graph' => array(
+            'checkbox',
+            'input[name="vhidden"]'
+        )
+    );
+
+    /**
+     *
+     * @var type 
+     */
+    protected $listingClass = '\Centreon\Test\Behat\Monitoring\MetricsConfigurationListingPage';
+
+    /**
+     *  Navigate to and/or check that we are on a contact configuration
+     *  page.
+     *
+     * @param $context  Centreon context.
+     * @param bool $visit True to navigate to a blank configuration page.
+     */
+    public function __construct($context, $visit = true)
+    {
+        // Visit page.
+        $this->context = $context;
+        if ($visit) {
+            $this->context->visit('main.php?p=20408&o=a');
+        }
+
+        // Check that page is valid for this class.
+        $mythis = $this;
+        $this->context->spin(
+            function ($context) use ($mythis) {
+                return $mythis->isPageValid();
+            },
+            'Current page does not match class ' . __CLASS__
+        );
+    }
+}
