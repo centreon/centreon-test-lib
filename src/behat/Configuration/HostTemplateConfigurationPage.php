eaf031428b997e3253d4b4784a8c5f4ebd4418b0
mariegallardo <gallardo.marie@hotmail.com>
2017-07-20 10:03:57 +0200

    All host properties handled (#38)
    * All properties handled
    
    * * fix style

--- src/behat/Configuration/HostTemplateConfigurationPage.php
+++ src/behat/Configuration/HostTemplateConfigurationPage.php
@@ -44,6 +44,16 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
             'input[name="host_address"]',
             self::CONFIGURATION_TAB
         ),
+        'snmp_community' => array(
+            'input',
+            'input[name="host_snmp_community"]',
+            self::CONFIGURATION_TAB
+        ),
+        'snmp_version' => array(
+            'select',
+            'select[name="host_snmp_version"]',
+            self::CONFIGURATION_TAB
+        ),
         'macros' => array(
             'custom',
             'Macros',
@@ -62,6 +72,16 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
         'check_command' => array(
             'select2',
             'select#command_command_id',
+            self::CONFIGURATION_TAB
+        ),
+        'command_arguments' => array(
+            'input',
+            'input[name="command_command_id_arg1"]',
+            self::CONFIGURATION_TAB
+        ),
+        'check_period' => array(
+            'select2',
+            'select#timeperiod_tp_id',
             self::CONFIGURATION_TAB
         ),
         'max_check_attempts' => array(
@@ -95,6 +115,31 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
             'input[name="host_notifications_enabled[host_notifications_enabled]"]',
             self::NOTIFICATION_TAB
         ),
+        'contacts' => array(
+            'select2',
+            'select[name="host_cs[]"]',
+            self::NOTIFICATION_TAB
+        ),
+        'contact_additive_inheritance' => array(
+            'radio',
+            'input[name="contact_additive_inheritance"]',
+            self::NOTIFICATION_TAB
+        ),
+        'contact_groups' => array(
+            'select2',
+            'select[name="host_cgs[]"]',
+            self::NOTIFICATION_TAB
+        ),
+        'contact_group_additive_inheritance' => array(
+            'radio',
+            'input[name="cg_additive_inheritance"]',
+            self::NOTIFICATION_TAB
+        ),
+        'notify_on_unreachable' => array(
+            'checkbox',
+            'input[name="host_notifOpts[u]"]',
+            self::NOTIFICATION_TAB
+        ),
         'notify_on_recovery' => array(
             'checkbox',
             'input[name="host_notifOpts[r]"]',
@@ -103,6 +148,21 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
         'notify_on_down' => array(
             'checkbox',
             'input[name="host_notifOpts[d]"]',
+            self::NOTIFICATION_TAB
+        ),
+        'notify_on_flapping' => array(
+            'checkbox',
+            'input[name="host_notifOpts[f]"]',
+            self::NOTIFICATION_TAB
+        ),
+        'notify_on_downtime_scheduled' => array(
+            'checkbox',
+            'input[name="host_notifOpts[s]"]',
+            self::NOTIFICATION_TAB
+        ),
+        'notify_on_none' => array(
+            'checkbox',
+            'input[name="host_notifOpts[r]"]',
             self::NOTIFICATION_TAB
         ),
         'notification_interval' => array(
@@ -136,16 +196,146 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
             'select#host_svTpls',
             self::RELATIONS_TAB
         ),
+        'parent_host_categories' => array(
+            'select2',
+            'select#host_hcs',
+            self::RELATIONS_TAB
+        ),
         // Data tab.
+        'obsess_over_host' => array(
+            'radio',
+            'input[name="host_obsess_over_host[host_obsess_over_host]"]',
+            self::DATA_TAB
+        ),
         'acknowledgement_timeout' => array(
             'input',
             'input[name="host_acknowledgement_timeout"]',
             self::DATA_TAB
         ),
+        'check_freshness' => array(
+            'radio',
+            'input[name="host_check_freshness[host_check_freshness]"]',
+            self::DATA_TAB
+        ),
+        'freshness_threshold' => array(
+            'input',
+            'input[name="host_freshness_threshold"]',
+            self::DATA_TAB
+        ),
+        'flap_detection_enabled' => array(
+            'radio',
+            'input[name="host_flap_detection_enabled[host_flap_detection_enabled]"]',
+            self::DATA_TAB
+        ),
+        'low_flap_threshold' => array(
+            'input',
+            'input[name="host_low_flap_threshold"]',
+            self::DATA_TAB
+        ),
+        'high_flap_threshold' => array(
+            'input',
+            'input[name="host_high_flap_threshold"]',
+            self::DATA_TAB
+        ),
+        'retain_status_information' => array(
+            'radio',
+            'input[name="host_retain_status_information[host_retain_status_information]"]',
+            self::DATA_TAB
+        ),
+        'retain_non_status_information' => array(
+            'radio',
+            'input[name="host_retain_nonstatus_information[host_retain_nonstatus_information]"]',
+            self::DATA_TAB
+        ),
+        'stalking_option_on_up' => array(
+            'checkbox',
+            'input[name="host_stalOpts[o]"]',
+            self::DATA_TAB
+        ),
+        'stalking_option_on_down' => array(
+            'checkbox',
+            'input[name="host_stalOpts[d]"]',
+            self::DATA_TAB
+        ),
+        'stalking_option_on_unreachable' => array(
+            'checkbox',
+            'input[name="host_stalOpts[u]"]',
+            self::DATA_TAB
+        ),
+        'event_handler_enabled' => array(
+            'radio',
+            'input[name="host_event_handler_enabled[host_event_handler_enabled]"]',
+            self::DATA_TAB
+        ),
+        'event_handler' => array(
+            'select2',
+            'select#command_command_id2',
+            self::DATA_TAB
+        ),
+        'event_handler_arguments' => array(
+            'input',
+            'input[name="command_command_id_arg2"]',
+            self::DATA_TAB
+        ),
         //Extended tab
+        'url' => array(
+            'input',
+            'input[name="ehi_notes_url"]',
+            self::EXTENDED_TAB
+        ),
+        'notes' => array(
+            'input',
+            'input[name="ehi_notes"]',
+            self::EXTENDED_TAB
+        ),
+        'action_url' => array(
+            'input',
+            'input[name="ehi_action_url"]',
+            self::EXTENDED_TAB
+        ),
+        'icon' => array(
+            'select',
+            'select#ehi_icon_image',
+            self::EXTENDED_TAB
+        ),
+        'alt_icon' => array(
+            'input',
+            'input[name="ehi_icon_image_alt"]',
+            self::EXTENDED_TAB
+        ),
+        'vrml_image' => array(
+            'select',
+            'select#ehi_vrml_image',
+            self::EXTENDED_TAB
+        ),
+        'status_map_image' => array(
+            'select',
+            'select#ehi_statusmap_image',
+            self::EXTENDED_TAB,
+        ),
+        '2d_coords' => array(
+            'input',
+            'input[name="ehi_2d_coords"]',
+            self::EXTENDED_TAB
+        ),
+        '3d_coords' => array(
+            'input',
+            'input[name="ehi_3d_coords"]',
+            self::EXTENDED_TAB
+        ),
+        'severity_level' => array(
+            'select',
+            'select[name="criticality_id"]',
+            self::EXTENDED_TAB
+        ),
         'enabled' => array(
             'radio',
             'input[name="host_activate[host_activate]"]',
+            self::EXTENDED_TAB
+        ),
+        'comments' => array(
+            'input',
+            'textarea[name="host_comment"]',
             self::EXTENDED_TAB
         )
     );
@@ -158,10 +348,10 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
     /**
      *  Host template edit page.
      *
-     *  @param $context  Centreon context object.
-     *  @param $visit    True to navigate to a blank edit page.
+     * @param $context  Centreon context object.
+     * @param $visit    True to navigate to a blank edit page.
      */
-    public function __construct($context, $visit = TRUE)
+    public function __construct($context, $visit = true)
     {
         // Visit page.
         $this->context = $context;
@@ -182,7 +372,7 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
     /**
      *  Get macros.
      *
-     *  @return macros
+     * @return macros
      */
     protected function getMacros()
     {
@@ -192,7 +382,7 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
         while (true) {
             $name = $this->context->getSession()->getPage()->findField('macroInput_' . $i);
             if (is_null($name)) {
-                break ;
+                break;
             }
             $value = $this->context->assertFindField('macroValue_' . $i);
             $macros[$name->getValue()] = $value->getValue();
@@ -205,13 +395,16 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
     /**
      *  Get host templates.
      *
-     *  @return host templates
+     * @return host templates
      */
     protected function getTemplates()
     {
         $templates = array();
 
-        $elements = $this->context->getSession()->getPage()->findAll('css', '[id^="tpSelect_"] option[selected="selected"]');
+        $elements = $this->context->getSession()->getPage()->findAll(
+            'css',
+            '[id^="tpSelect_"] option[selected="selected"]'
+        );
         foreach ($elements as $element) {
             $templateName = $element->getText();
             if ($templateName != '') {
@@ -225,14 +418,14 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
     /**
      *  Set macros.
      *
-     *  @param $macros Macros.
+     * @param $macros Macros.
      */
     protected function setMacros($macros)
     {
         $currentMacros = $this->getMacros();
         $i = count($currentMacros);
         foreach ($macros as $name => $value) {
-            $this->context->assertFind('css' , '#macro_add p')->click();
+            $this->context->assertFind('css', '#macro_add p')->click();
             $this->context->assertFindField('macroInput_' . $i)->setValue($name);
             $this->context->assertFindField('macroValue_' . $i)->setValue($value);
             $i++;
@@ -242,7 +435,7 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
     /**
      *  Set host templates.
      *
-     *  @param $templates  Parent templates.
+     * @param $templates  Parent templates.
      */
     protected function setTemplates($templates)
     {
@@ -257,5 +450,3 @@ class HostTemplateConfigurationPage extends \Centreon\Test\Behat\ConfigurationPa
         }
     }
 }
-
-?>
