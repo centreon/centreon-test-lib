a5625cf971aeb691e803876f7ae86b80b4a1a64b
Guillaume28 <guillaume-baldo@hotmail.fr>
2017-08-17 17:14:18 +0200

    New container for elasticsearch

--- src/behat/Extensions/ContainerExtension.php
+++ src/behat/Extensions/ContainerExtension.php
@@ -60,6 +60,7 @@ class ContainerExtension implements ExtensionInterface
                 ->scalarNode('lm_squid_simple')->defaultValue('mon-lm-squid-simple-dev.yml')->end()
                 ->scalarNode('lm_squid_basic_auth')->defaultValue('mon-lm-squid-basic-auth-dev.yml')->end()
                 ->scalarNode('web_influxdb')->defaultValue('mon-web-influxdb.yml')->end()
+                ->scalarNode('web_elasticsearch')->defaultValue('mon-web-elasticsearch.yml')->end()
                 ->scalarNode('lm')->defaultValue('mon-lm-dev.yml')->end()
                 ->scalarNode('poller-display')->defaultValue('mon-poller-display-dev.yml')->end()
                 ->scalarNode('ppe')->defaultValue('mon-ppe-dev.yml')->end()
@@ -86,5 +87,3 @@ class ContainerExtension implements ExtensionInterface
     {
     }
 }
-
-?>
