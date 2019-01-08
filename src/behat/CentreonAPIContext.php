bcd11ac24371a075e61cb2a0091e6e681f2d9f2d
Victor Vassilev <victor.vassilev@stanga.net>
2019-01-08 13:10:09 +0200

    finalize API context v1

--- src/behat/CentreonAPIContext.php
+++ src/behat/CentreonAPIContext.php
@@ -32,6 +32,11 @@ class CentreonAPIContext extends CentreonContext
     protected $response;
 
     /**
+     * @var string
+     */
+    protected $authToken;
+
+    /**
      * Constructor
      *
      * @param array $parameters The list of parameters given in behat.yml
@@ -39,9 +44,22 @@ class CentreonAPIContext extends CentreonContext
     public function __construct($parameters = array())
     {
         parent::__construct($parameters);
-        $baseUrl = $parameters['base_url'];
-        $client = new Client(['base_url' => $baseUrl]);
-        $this->client = $client;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthToken(): string
+    {
+        return $this->authToken;
+    }
+
+    /**
+     * @param string $authToken
+     */
+    public function setAuthToken(string $authToken): void
+    {
+        $this->authToken = $authToken;
     }
 
     /**
@@ -77,11 +95,36 @@ class CentreonAPIContext extends CentreonContext
     }
 
     /**
+     * Instantiate the http client for testing
+     *
+     * @Given I have a running instance of Centreon API
+     * @throws \Exception
+     */
+    public function iHaveRunningAPI()
+    {
+        $base_url = $this->getMinkParameter('api_base');
+        if (is_null($base_url))
+        {
+            throw new \Exception('Unable to find a running container with Centreon Web');
+        }
+
+        $client = new Client(['base_url' => $base_url]);
+        $this->setClient($client);
+        $this->authenticateToApi();
+        $headers = [
+            'centreon-auth-token' => $this->getAuthToken(),
+            'Content-Type' => 'application/json'
+        ];
+        $client = new Client(['headers' => $headers]);
+        $this->setClient($client);
+    }
+
+    /**
      * @When /^I make a GET request to "([^"]*)"$/
      */
     public function makeGetRequest($uri)
     {
-        $response = $this->getClient()->get($uri);
+        $response = $this->getClient()->request('GET', $this->getMinkParameter('api_base') . $uri);
         $this->setResponse($response);
     }
 
@@ -90,18 +133,51 @@ class CentreonAPIContext extends CentreonContext
      */
     public function makePostRequest($uri)
     {
-        $response = $this->getClient()->get($uri);
-        $this->setResponse($response);
+        return true;
     }
 
     /**
      * @Then /^the response code should be (\d+)$/
+     * @throws \Exception
      */
     public function theResponseCodeShouldBe($code)
     {
-        if (intval($this->getResponse()->getStatusCode()) !== $code) {
+        if ((string) $this->getResponse()->getStatusCode() !== $code) {
             throw new \Exception('HTTP response code does not match '.$code.
                 ' (returned: '.$this->getResponse()->getStatusCode().')');
         }
+    }
+
+    /**
+     * @Given /^the response has a "([^"]*)" property$/
+     */
+    public function responseHasProperty($property)
+    {
+        $data = json_decode($this->getResponse()->getBody(true));
+        if (!empty($data)) {
+            if (!isset($data->$property)) {
+                throw new Exception("Property '".$property."' is not found!\n");
+            }
+        } else {
+            throw new Exception("Response not JSON\n" . $this->getResponse()->getBody(true));
+        }
+    }
+    /**
+     * @throws \Exception
+     */
+    private function authenticateToApi()
+    {
+        $client = $this->getClient();
+        $response = $client->post($this->getMinkParameter('api_base') . '/api/index.php?action=authenticate', [
+            'form_params' => [
+                'username' => 'admin',
+                'password' => 'centreon'
+            ]
+        ]);
+        $responseObj = json_decode($response->getBody());
+        if (empty($responseObj->authToken)){
+            throw new \Exception('Could not get authentication token from API.');
+        }
+        $this->setAuthToken($responseObj->authToken);
     }
 }
