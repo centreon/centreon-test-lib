e0c5705ede21324dbb15b67c6560258644d8a382
Kevin Duret <kduret@centreon.com>
2020-03-10 13:57:35 +0100

    fix coding style

--- src/PHPStan/AbsolutePathErrorFormatter.php
+++ src/PHPStan/AbsolutePathErrorFormatter.php
@@ -18,7 +18,8 @@
  * For more information : contact@centreon.com
  *Controller
  */
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace Centreon\PHPStan;
 
@@ -33,123 +34,122 @@ use \PHPStan\Command\Output;
  */
 class AbsolutePathErrorFormatter implements ErrorFormatter
 {
-	/**
-	 * Format errors output
-	 *
-	 * @param AnalysisResult $analysisResult Result of check style
-	 * @param Output $output Output stream to write
-	 * @return integer If there are some errors
-	 */
-	public function formatErrors(
-		AnalysisResult $analysisResult,
-		Output $output
-	): int
-	{
-		$output->writeRaw('<?xml version="1.0" encoding="UTF-8"?>');
-		$output->writeLineFormatted('');
-		$output->writeRaw('<checkstyle>');
-		$output->writeLineFormatted('');
+    /**
+     * Format errors output
+     *
+     * @param AnalysisResult $analysisResult Result of check style
+     * @param Output $output Output stream to write
+     * @return integer If there are some errors
+     */
+    public function formatErrors(
+        AnalysisResult $analysisResult,
+        Output $output
+    ): int {
+        $output->writeRaw('<?xml version="1.0" encoding="UTF-8"?>');
+        $output->writeLineFormatted('');
+        $output->writeRaw('<checkstyle>');
+        $output->writeLineFormatted('');
 
-		foreach ($this->groupByFile($analysisResult) as $filePath => $errors) {
-			$filePath = $this->parseFilePath($filePath);
-			$output->writeRaw(sprintf(
-				'<file name="%s">',
-				$filePath
-			));
-			$output->writeLineFormatted('');
+        foreach ($this->groupByFile($analysisResult) as $filePath => $errors) {
+            $filePath = $this->parseFilePath($filePath);
+            $output->writeRaw(sprintf(
+                '<file name="%s">',
+                $filePath
+            ));
+            $output->writeLineFormatted('');
 
-			foreach ($errors as $error) {
-				$output->writeRaw(sprintf(
-					'  <error line="%d" column="1" severity="error" message="%s" source="PHPStan" />',
-					$this->escape((string) $error->getLine()),
-					$this->escape((string) $error->getMessage())
-				));
-				$output->writeLineFormatted('');
-			}
-			$output->writeRaw('</file>');
-			$output->writeLineFormatted('');
-		}
+            foreach ($errors as $error) {
+                $output->writeRaw(sprintf(
+                    '  <error line="%d" column="1" severity="error" message="%s" source="PHPStan" />',
+                    $this->escape((string) $error->getLine()),
+                    $this->escape((string) $error->getMessage())
+                ));
+                $output->writeLineFormatted('');
+            }
+            $output->writeRaw('</file>');
+            $output->writeLineFormatted('');
+        }
 
-		$notFileSpecificErrors = $analysisResult->getNotFileSpecificErrors();
+        $notFileSpecificErrors = $analysisResult->getNotFileSpecificErrors();
 
-		if (count($notFileSpecificErrors) > 0) {
-			$output->writeRaw('<file>');
-			$output->writeLineFormatted('');
+        if (count($notFileSpecificErrors) > 0) {
+            $output->writeRaw('<file>');
+            $output->writeLineFormatted('');
 
-			foreach ($notFileSpecificErrors as $error) {
-				$output->writeRaw(
-					sprintf('  <error severity="error" message="%s" source="PHPStan" />', $this->escape($error))
-				);
-				$output->writeLineFormatted('');
-			}
+            foreach ($notFileSpecificErrors as $error) {
+                $output->writeRaw(
+                    sprintf('  <error severity="error" message="%s" source="PHPStan" />', $this->escape($error))
+                );
+                $output->writeLineFormatted('');
+            }
 
-			$output->writeRaw('</file>');
-			$output->writeLineFormatted('');
-		}
+            $output->writeRaw('</file>');
+            $output->writeLineFormatted('');
+        }
 
-		if ($analysisResult->hasWarnings()) {
-			$output->writeRaw('<file>');
-			$output->writeLineFormatted('');
+        if ($analysisResult->hasWarnings()) {
+            $output->writeRaw('<file>');
+            $output->writeLineFormatted('');
 
-			foreach ($analysisResult->getWarnings() as $warning) {
-				$output->writeRaw(
-					sprintf('  <error severity="warning" message="%s" source="PHPStan" />', $this->escape($warning))
-				);
-				$output->writeLineFormatted('');
-			}
+            foreach ($analysisResult->getWarnings() as $warning) {
+                $output->writeRaw(
+                    sprintf('  <error severity="warning" message="%s" source="PHPStan" />', $this->escape($warning))
+                );
+                $output->writeLineFormatted('');
+            }
 
-			$output->writeRaw('</file>');
-			$output->writeLineFormatted('');
-		}
+            $output->writeRaw('</file>');
+            $output->writeLineFormatted('');
+        }
 
-		$output->writeRaw('</checkstyle>');
-		$output->writeLineFormatted('');
+        $output->writeRaw('</checkstyle>');
+        $output->writeLineFormatted('');
 
-		return $analysisResult->hasErrors() ? 1 : 0;
-	}
+        return $analysisResult->hasErrors() ? 1 : 0;
+    }
 
-	/**
-	 * Escapes values for using in XML
-	 *
-	 * @param string $string
-	 * @return string
-	 */
-	protected function escape(string $string): string
-	{
-		return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, 'UTF-8');
-	}
+    /**
+     * Escapes values for using in XML
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function escape(string $string): string
+    {
+        return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, 'UTF-8');
+    }
 
-	/**
-	 * Group errors by file
-	 *
-	 * @param AnalysisResult $analysisResult
-	 * @return array<string, array> Array that have as key the absolute path of file
-	 *                              and as value an array with occured errors.
-	 */
-	private function groupByFile(AnalysisResult $analysisResult): array
-	{
-		$files = [];
+    /**
+     * Group errors by file
+     *
+     * @param AnalysisResult $analysisResult
+     * @return array<string, array> Array that have as key the absolute path of file
+     *                              and as value an array with occured errors.
+     */
+    private function groupByFile(AnalysisResult $analysisResult): array
+    {
+        $files = [];
 
-		/** @var \PHPStan\Analyser\Error $fileSpecificError */
-		foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
-			$files[$fileSpecificError->getFile()][] = $fileSpecificError;
-		}
+        /** @var \PHPStan\Analyser\Error $fileSpecificError */
+        foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
+            $files[$fileSpecificError->getFile()][] = $fileSpecificError;
+        }
 
-		return $files;
-	}
+        return $files;
+    }
 
-	/**
-	 * Remove useless information like trait context
-	 *
-	 * @param string $filePath Absolute file path
-	 * @return string File path with removed useless information
-	 */
-	private function parseFilePath(string $filePath): string
-	{
-		if (preg_match('/(.+)\s+\(in context/', $filePath, $matches)) {
-			$filePath = $matches[1];
-		}
+    /**
+     * Remove useless information like trait context
+     *
+     * @param string $filePath Absolute file path
+     * @return string File path with removed useless information
+     */
+    private function parseFilePath(string $filePath): string
+    {
+        if (preg_match('/(.+)\s+\(in context/', $filePath, $matches)) {
+            $filePath = $matches[1];
+        }
 
-		return $this->escape($filePath);
-	}
+        return $this->escape($filePath);
+    }
 }
