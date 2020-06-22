57464359a9a309abe372b4d6903c54c9bcc11548
schapron <schapron@centreon.com>
2020-06-22 17:36:22 +0200

    add resource context

--- src/behat/Api/Context/ApiContext.php
+++ src/behat/Api/Context/ApiContext.php
@@ -463,4 +463,22 @@ class ApiContext implements Context
             10
         );
     }
+
+    /**
+     * Wait service to be monitored
+     *
+     * @Given I wait until service :service is monitored
+     */
+    public function iWaitUntilServiceIsMonitored(string $resource)
+    {
+        $this->spin(
+            function() use ($resource) {
+                $this->iSendARequestTo('GET', '/beta/monitoring/services?search={"service.description":"' . $resource . '"}');
+                $this->theJsonNodeShouldHaveElements('result', 1);
+                return true;
+            },
+            'the resource ' . $resource . ' seems not monitored',
+            10
+        );
+    }
 }
