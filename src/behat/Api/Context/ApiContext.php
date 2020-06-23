708b24f9d44b658382762cbaa7ee1fd795f645e9
schapron <schapron@centreon.com>
2020-06-23 11:45:55 +0200

    move context to centreon repo

--- src/behat/Api/Context/ApiContext.php
+++ src/behat/Api/Context/ApiContext.php
@@ -463,22 +463,4 @@ class ApiContext implements Context
             10
         );
     }
-
-    /**
-     * Wait service to be monitored
-     *
-     * @Given I wait until service :service is monitored
-     */
-    public function iWaitUntilServiceIsMonitored(string $resource)
-    {
-        $this->spin(
-            function() use ($resource) {
-                $this->iSendARequestTo('GET', '/beta/monitoring/services?search={"service.description":"' . $resource . '"}');
-                $this->theJsonNodeShouldHaveElements('result', 1);
-                return true;
-            },
-            'the resource ' . $resource . ' seems not monitored',
-            10
-        );
-    }
 }
