a6b7226d47ca3998618a78cb9a6ce1372fc590b4
Kevin Duret <kduret@centreon.com>
2022-10-11 08:47:26 +0200

    fix

--- src/behat/RestApiContext.php
+++ src/behat/RestApiContext.php
@@ -48,7 +48,7 @@ class RestApiContext extends CentreonContext
     public function aCentreonServerWithRestApiTestingData()
     {
         // Launch container.
-        $this->launchCentreonWebContainer(__DIR__ . '/../../../../../docker-compose.yml', ['web-fresh']);
+        $this->launchCentreonWebContainer('docker-compose-web', ['web-fresh']);
 
         // Copy images.
         $basedir = 'tests/rest_api/images';
