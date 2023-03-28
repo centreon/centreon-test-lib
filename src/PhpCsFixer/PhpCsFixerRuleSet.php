219c56603f5ba534257dd6fa5ac2a68f79b7de5f
abuathier <114949454+abuathier@users.noreply.github.com>
2023-03-28 13:54:59 +0200

    feat(php-cs-fixer): dynamic headers in code style (#226)

--- src/PhpCsFixer/PhpCsFixerRuleSet.php
+++ src/PhpCsFixer/PhpCsFixerRuleSet.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,18 +19,38 @@
  *
  */
 
+declare(strict_types=1);
+
 namespace Centreon\PhpCsFixer;
 
 class PhpCsFixerRuleSet
 {
+    private const YEAR = 2023;
+    private const LICENSE_APACHE = 'apache';
+    private const LICENSE_PRIVATE = 'private';
+    private const LICENSES = [
+        'centreon/centreon-test-lib' => self::LICENSE_APACHE,
+        'centreon/centreon' => self::LICENSE_APACHE,
+        'centreon/centreon-anomaly-detection' => self::LICENSE_PRIVATE,
+        'centreon/centreon-autodiscovery' => self::LICENSE_PRIVATE,
+        'centreon/centreon-bam' => self::LICENSE_PRIVATE,
+        'centreon/centreon-cloud-business-extensions' => self::LICENSE_PRIVATE,
+        'centreon/centreon-cloud-extensions' => self::LICENSE_PRIVATE,
+        'centreon/centreon-it-edition-extensions' => self::LICENSE_PRIVATE,
+        'centreon/centreon-license-manager' => self::LICENSE_PRIVATE,
+        'centreon/centreon-map' => self::LICENSE_PRIVATE,
+        'centreon/centreon-mbi' => self::LICENSE_PRIVATE,
+        'centreon/centreon-pp-manager' => self::LICENSE_PRIVATE,
+    ];
+
     /**
-     * This method returns an array of defined rules Php-Cs-Fixer
+     * This method returns an array of defined rules Php-Cs-Fixer.
      *
      * @return array
      */
     public static function getRules(): array
     {
-        return [
+        $rules = [
             'align_multiline_comment' => true,
             'array_indentation' => true,
             'array_push' => true,
@@ -46,8 +66,8 @@ class PhpCsFixerRuleSet
                     'property' => 'one',
                     'const' => 'only_if_meta',
                     'trait_import' => 'none',
-                    'case' => 'none'
-                ]
+                    'case' => 'none',
+                ],
             ],
             'clean_namespace' => true,
             'combine_consecutive_issets' => true,
@@ -97,7 +117,7 @@ class PhpCsFixerRuleSet
                     'square_brace_block',
                     'switch',
                     'throw',
-                    'use'
+                    'use',
                 ],
             ],
             'no_homoglyph_names' => true,
@@ -114,7 +134,7 @@ class PhpCsFixerRuleSet
                     'array',
                     'array_destructuring',
                     'group_import',
-                ]
+                ],
             ],
             'no_trailing_whitespace_in_string' => true,
             'no_unneeded_control_parentheses' => true,
@@ -193,5 +213,86 @@ class PhpCsFixerRuleSet
             'whitespace_after_comma_in_array' => true,
             'declare_strict_types' => true,
         ];
+
+        // Set the header dynamically based on the current detected project name.
+        $projectLicense = self::detectCentreonProjectLicense(__DIR__);
+        if ($phpLicenseHeader = self::getLicenseHeader($projectLicense)) {
+            $rules += [
+                'header_comment' => [
+                    'location' => 'after_open',
+                    'header' => $phpLicenseHeader,
+                ],
+            ];
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Return the header based on the project license type.
+     *
+     * @param string $projectLicense
+     */
+    private static function getLicenseHeader(string $projectLicense): string
+    {
+        $content = match ($projectLicense) {
+            self::LICENSE_APACHE => (string) file_get_contents(__DIR__ . '/license.apache.txt'),
+            self::LICENSE_PRIVATE => (string) file_get_contents(__DIR__ . '/license.private.txt'),
+            default => '',
+        };
+
+        return str_replace('{YEAR}', (string) self::YEAR, $content);
+    }
+
+    /**
+     * Recursively find the root main project which uses this project in its vendor.
+     *
+     * @param string $directory
+     *
+     * @return string
+     */
+    private static function detectCentreonProjectLicense(string $directory): string
+    {
+        // "end" conditions -> '', '.', '/'
+        while (\mb_strlen($directory) > 1) {
+            if (
+                // A composer.json file is mandatory.
+                is_file($composerFile = $directory . '/composer.json')
+
+                // Avoid a composer.json from inside the Centreon vendor directory.
+                && ! str_ends_with(dirname($directory), '/vendor/centreon')
+
+                // There should have a defined license.
+                && ($license = self::getCentreonProjectLicense($composerFile))
+            ) {
+                return $license;
+            }
+
+            $directory = dirname($directory);
+        }
+
+        return '';
+    }
+
+    /**
+     * Extract the project name from the composer json file
+     * and then return the related license if defined.
+     *
+     * @param string $composerFile
+     */
+    private static function getCentreonProjectLicense(string $composerFile): ?string
+    {
+        try {
+            $composerContent = (string) file_get_contents($composerFile);
+            $composerData = json_decode($composerContent, true, 512, JSON_THROW_ON_ERROR);
+            $projectName = $composerData['name'] ?? null;
+
+            if ($projectName && isset(self::LICENSES[$projectName])) {
+                return self::LICENSES[$projectName];
+            }
+        } catch (\JsonException) {
+        }
+
+        return null;
     }
 }
