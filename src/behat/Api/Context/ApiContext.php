802ccb303628d7601051428ca0c31f8c24795732
Kevin Duret <kduret@centreon.com>
2023-08-03 12:50:37 +0200

    fix(api): reload acl on debian with www-data user

--- src/behat/Api/Context/ApiContext.php
+++ src/behat/Api/Context/ApiContext.php
@@ -683,7 +683,7 @@ class ApiContext implements Context
      * @param int $tries Count of tries
      * @return int the count of results
      *
-     * @Given /^I wait to get (\d+) results? from "(\S+)"(?: \(tries: (\d+)\))?$/
+     * @Given /^I wait to get (\d+) results? from ['"](\S+)['"](?: \(tries: (\d+)\))?$/
      */
     public function iWaitToGetSomeResultsFrom(int $count, string $url, int $tries = 15): int
     {
