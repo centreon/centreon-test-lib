6deeb78ca8d9963d4565a7b1d8a6b6a0a17db4ec
jeremyjaouen <jjaouen@centreon.com>
2024-08-30 10:48:17 +0200

    remove broken flag

--- src/behat/CentreonContext.php
+++ src/behat/CentreonContext.php
@@ -33,6 +33,7 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Mink\Mink;
 use Behat\Mink\Session;
 use Behat\Mink\Driver\PantherDriver;
+use Symfony\Component\Panther\PantherTestCase;
 
 class CentreonContext extends UtilsContext
 {
@@ -85,7 +86,6 @@ class CentreonContext extends UtilsContext
                 '--no-experiments',
                 '--no-first-run',
                 '--no-initial-navigation',
-                '--no-startup-window',
                 '--no-wifi',
                 '--suppress-message-center-popups',
                 '--disable-extensions',
@@ -107,7 +107,7 @@ class CentreonContext extends UtilsContext
             $defaultOptions = [
                 'external_base_uri' => 'http://' . $this->container->getHost() . ':'
                     . $this->container->getPort(80, $this->webService),
-                'browser' => 'chrome',
+                'browser' => PantherTestCase::CHROME,
             ];
 
             $kernelOptions = []; # unused cause we do not extend class KernelTestCase
@@ -126,7 +126,7 @@ class CentreonContext extends UtilsContext
             $driver = new PantherDriver($defaultOptions, $kernelOptions, $managerOptions);
             $driver->start();
         } catch (\Exception $e) {
-            throw new \Exception("Cannot instantiate panther driver : " . $e->getMessage(), (int) $e->getCode(), $e);
+            throw new \Exception("Cannot instantiate panther driver : " . $e->getTraceAsString(), (int) $e->getCode(), $e);
         }
 
         try {
